86b0d173e922de68bcfa5683a40ead028813a54c
roxana perrotta <roxanatperrotta@gmail.com>
2023-08-24 20:00:42 -0300

    nuevocambio

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,25 @@
 
+//Definición objeto Promociones
+
+function promociones (producto, bebida, precio)
+{
+    this.producto=producto;
+    this.bebida=bebida;
+    this.precio=precio;
+}
+   
+//Definición promociones 
+
+const promocion1 = new promociones("24 empanadas", "gaseosa de 2lts", 7000);
+const promocion2 = new promociones("2 pizzas", "gaseosa de 2lts", 7500);
+const promocion3 = new promociones("1 pizza y 12 empanadas", "gaseosa de 2lts", 8000);
+
+
+//Creación del carrito
+
+const carrito = []
+
+// Función para saludar e inicio del simulador
 
 function saludar (){
     alert ("Bienvenido a Ruta 11 - Casa de Comidas");
@@ -23,25 +44,30 @@ let ingreso1= prompt ("Hola " + (ingreso) + "! Ingresa 1 para elegir tu promo, o
         for (let i=0; i<3; i++){
 
 
-        let respuesta= prompt ("PROMOS AGOSTO \n Ingresa una opción. \n 1 - Dos muzzarellas y una gaseosa de 2 litros $6500 \n 2- Dos docenas de empanadas de carne y una cerveza de litro $7000 \n 3- Una muzarella y una docena de empanadas, más una gaseosa de 2 litros $7500");
-    
-        let num1= 6500;
-        let num2= 7000;
-        let num3= 7500;
+        let respuesta= prompt ("PROMOS AGOSTO \n Ingresa una opción. \n 1 -24 empanadas y 1 gaseosa de 2 lts, $7000 \n 2- 2 muzarellas y 1 gaseosa de 2 litros $7500 \n 3- 1 muzarella y 12 empanadas, más 1 gaseosa de 2 litros $8000");
     
 
         switch (respuesta){
 
             case '1':
-            alert ("Seleccionaste la promo 1: Dos muzarellas y una gaseosa de dos litros $" +num1+ " Se ha agregado a tu pedido.");
+            alert ("Seleccionaste la promo 1: 24 empanadas y 1 gaseosa de 2 lts, $7000. Esta promo se ha agregado a tu pedido.");
+
+            carrito.push (promocion1);
+
             break;
         
             case '2':
-            alert ("Seleccionaste la promo 2: Dos docenas de empanadas de carne y una cerveza de litro $" +num2 + " Se ha agregado a tu pedido");
+            alert ("Seleccionaste la promo 2: 2 muzarellas y 1 gaseosa de 2 litros $7500. Esta promo se ha agregado a tu pedido");
+
+            carrito.push (promocion2);
+
             break;
 
             case '3':
-            alert ("Seleccionaste la promo 3: una muzarella y una docena de empanadas, más una gaseosa de 2 litros $" +num3+ " Se ha agregado a tu pedido");
+            alert ("Seleccionaste la promo 3: 1 muzarella y 12 empanadas, más 1 gaseosa de 2 litros $8000. Esta promo se ha agregado a tu pedido");
+
+            carrito.push (promocion3);
+
             break;
 
             default:
@@ -58,12 +84,21 @@ let ingreso1= prompt ("Hola " + (ingreso) + "! Ingresa 1 para elegir tu promo, o
         }
     }   
 
-    alert ("El total de tu pedido es...");
-    alert ("Gracias por elegirnos! Qué lo disfrutes");
-    }
+    alert ("Promociones en el carrito:");
     
+    carrito.forEach (promociones =>{alert(`${promociones.producto} - $${promociones.precio}`);});
 
     
+    const totalCarrito = carrito.reduce((acc,promociones)=> {
+        return acc + promociones.precio
+    
+    }, 0);
+
+    alert(`El total de tu pedido es: $${totalCarrito}`);
+
+
+    alert ("Gracias por elegirnos! Qué lo disfrutes");
+    }
     
         
    
