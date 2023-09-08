d82e3d7afd72827d4d234cc7378cf42cd30914ba
roxana perrotta <roxanatperrotta@gmail.com>
2023-09-07 23:20:52 -0300

    ultimo cambio

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,3 @@
-
-
 //Definición objeto Promociones
 
 function promociones (producto, bebida, precio)
@@ -18,33 +16,154 @@ const promocion3 = new promociones("1 pizza y 12 empanadas", "gaseosa de 2lts", 
 
 //Creación del carrito
 
-const carrito = []
+const carrito = [];
+localStorage.setItem('carrito',JSON.stringify(carrito))
 
+//Mostrar el carrito en DOM
+function mostrarmisProductos(carrito){
+const carritoLista=document.getElementById("micarrito");
+carritoLista.innerHTML="";
+for (const promociones of carrito){
+    const li=document.createElement("li");
+    li.textContent=`${promociones.producto}:${promociones.precio}`;
+    carritoLista.appendChild(li)
+}
+}
+//Calcular total carrito
 
-//Acceder al DOM
+/*function calculartotal(carrito){
+let total=0;
+    for (const promociones of carrito) {
+        total += promociones.precio; 
+    }
+    return total;
+}*/
+const totalCarrito = carrito.reduce((acc,promociones)=> {
+    return acc + promociones.precio
 
-const conten= document.getElementsByTagName("conten");
-console.log(conten);
+}, 0);
+
+ function mostrarPrecioTotal(totalCarrito){
+    const totalPrecio=document.getElementById("totalcarrito");
+    totalPrecio.textcontent=`El total de tu pedido es: $${total}`
+ } 
+//Mostrar total del carrito
+function totalMiCarrito(carrito){
+    const totalcarrito=document.getElementById("totalcarrito");
+    totalcarrito.innerHTML="";
+        const final=document.createElement("p");
+        final.textContent=`El total de tu pedido es: $${totalCarrito}`;
+        totalcarrito.appendChild(final)
+    }
 
 //Creación de las tarjetas de promos
 
 const div1=document.createElement("div");
-div1.innerHTML= `<div>
-    <h3>${promocion1.nombre}</h3>
-    <p>Precio$ ${promocion1.precio}</p>
-    <img src="./imagenes/Copia de promoempanadas.jpeg" alt="promocion de 24 empanadas">
+div1.innerHTML= `<div class="card">
+    <h3>${promocion1.producto}</h3>
+    <h3>$ ${promocion1.precio}</h3>
+    <img class="fotopromo" src="./imagenes/Copia de promoempanadas.jpeg" alt="promocion de 24 empanadas">
+    <button class=boton1> Agregar al Carrito</button>
 </div>`
+console.log(div1);
 
 document.body.append(div1);
 
 
 
+const div2=document.createElement("div");
+div2.innerHTML= `<div class="card">
+    <h3>${promocion2.producto}</h3>
+    <h3>$ ${promocion2.precio}</h3>
+    <img class="fotopromo" src="./imagenes/Copia de pizza2.jpeg" alt="promocion de 2 muzzarellas">
+    <button class=boton2> Agregar al Carrito</button>;
+</div>`
+console.log(div2);
+
+document.body.append(div2);
+
+const div3=document.createElement("div");
+div3.innerHTML= `<div class="card">
+    <h3>${promocion3.producto}</h3>
+    <h3>$ ${promocion3.precio}</h3>
+    <img class="fotopromo" src="./imagenes/Copia de pizza-y-empanadaspng.webp" alt="promocion de 1 muzzarella y 12 empanadas">
+    <button class=boton3> Agregar al Carrito</button>;
+</div>`
+
+
+console.log(div3);
+
+document.body.append(div3);
+
+const div4=document.createElement("div");
+div4.innerHTML= `<div>
+    <img class="fotocarrito" src="./imagenes/carro-de-la-compra.png" alt="tu carrito de compras">
+    <button class=boton4> finalizar pedido </button>;
+</div>`
+console.log(div4);
+
+document.body.append(div4);
+
+//Asignar eventos a los botones
+
+const boton= document.getElementsByClassName("boton1");
+console.log(boton)
+
+let botonpromo1=boton[0];
+
+botonpromo1.addEventListener('click', ()=>{
+   
+    carrito.push(promocion1);
+    mostrarmisProductos(carrito);
+    console.log(carrito);
+
+})
+
+const boton2= document.getElementsByClassName("boton2");
+console.log(boton2);
+
+let botonpromo2=boton2[0];
+
+botonpromo2.addEventListener('click', ()=>{
+    
+    carrito.push(promocion2);
+    mostrarmisProductos(carrito);
+})
+
+const boton3= document.getElementsByClassName("boton3");
+console.log(boton3);
+
+let botonpromo3=boton3[0];
+
+botonpromo3.addEventListener('click', ()=>{
+   
+    carrito.push(promocion3);
+    mostrarmisProductos(carrito);
+
+})
+const boton4= document.getElementsByClassName("boton4");
+console.log(boton4);
+
+let botonfinpedido=boton4[0];
+
+botonfinpedido.addEventListener('click', ()=>{
+
+    totalMiCarrito(carrito);
+})
 
 
 
 
 
 
+
+
+
+
+    /*alert(`El total de tu pedido es: $${totalCarrito}`);
+
+
+    alert ("Gracias por elegirnos! Qué lo disfrutes");*/
 
 
 
